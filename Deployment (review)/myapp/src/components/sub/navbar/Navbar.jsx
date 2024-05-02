09c5d2eed7fcdfd24a488091faa64fe3a985e80d
Kaviyarasu1234 <118583260+Kaviyarasu1234@users.noreply.github.com>
2024-05-02 02:52:40 -0700

    Add files via upload

---
+++ Deployment (review)/myapp/src/components/sub/navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import './Navbar.scss'
+import logo from '../../../assets/images/NIIT.png'
+import { useNavigate } from 'react-router-dom'
+
+function Navbar() {
+     var navigate = useNavigate();
+     return (
+          <div className='navbar-contents'>
+               <div className='logo'>
+                    <img src={logo} alt=''></img>
+               </div>
+
+               <div className='navelement'>
+                   <ul></ul>
+               </div>
+               <ul className='links'>
+                    <li><p onClick={()=>{navigate("/authorize")}}>SIGN IN</p></li>
+               </ul>
+          </div>
+     )
+}
+
+export default Navbar
